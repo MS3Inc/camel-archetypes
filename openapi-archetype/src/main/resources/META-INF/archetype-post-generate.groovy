54818d50b940c5850b8ddf093c77f62a8cdbbdab
R. L. Ratcliffe <rlr@tcliffe.dev>
2021-01-11 16:39:28 -0500

    Attempt to fix plugin not being resolved
    - Add version to mvn plugin command
    - Add debug flag for maven
    - Update with pluginRepositories

--- openapi-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ openapi-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -21,5 +21,5 @@ log.info("Attempting to execute mvn command...\n")
 def specUri = request.properties['specificationUri']
 def generatedApiDirectory = request.outputDirectory + "/" + request.artifactId
 
-def mvnCommand = "mvn com.ms3-inc.camel:camel-restdsl-openapi-plugin:generate -DspecificationUri=" + specUri + " -f " + generatedApiDirectory
+def mvnCommand = "mvn com.ms3-inc.camel:camel-restdsl-openapi-plugin:1.0-SNAPSHOT:generate -DspecificationUri=" + specUri + " -f " + generatedApiDirectory + " -X"
 log.info mvnCommand.execute().text
