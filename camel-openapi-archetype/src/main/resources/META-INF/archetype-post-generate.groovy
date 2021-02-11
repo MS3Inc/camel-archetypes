333b172fc88ca61c26d9cef22b05610f7f3f8b9a
R. L. Ratcliffe <rlr@tcliffe.dev>
2021-02-11 09:52:00 -0500

    Fix issues #6 and #8
    - Implement writing of .gitignore functionality
    - Update maven resources plugin version
    - Implement archetype failing when plugin fails

--- camel-openapi-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ camel-openapi-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -13,9 +13,36 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
 def log = LoggerFactory.getLogger('org.apache.camel.archetype')
+
+boolean isFailure = false;
+
+Path origGitIgnorePath = Paths.get(request.outputDirectory, request.artifactId, "gitignore");
+Path newGitIgnorePath = origGitIgnorePath.getParent().resolve(".gitignore");
+
+if (Files.exists(origGitIgnorePath)) {
+    try {
+        Files.move(origGitIgnorePath, newGitIgnorePath);
+        log.info("Created .gitignore...\n")
+    } catch (Exception e) {
+        log.error("Could not create .gitignore file. " + e.getMessage().toString())
+        isFailure = true;
+    }
+} else {
+    log.error("Could not find $origGitIgnorePath")
+    isFailure = true;
+}
+
+
+
 log.info("Attempting to execute mvn command...\n")
 
 def specUri = request.properties['specificationUri']
@@ -24,7 +51,7 @@ def generatedApiDirectory = request.outputDirectory + "/" + request.artifactId
 log.info("Running mvn in " + generatedApiDirectory)
 
 def prefixForRunningWithWindows =  ['cmd', '/c']
-def mvnCommand = ['mvn', 'com.ms3-inc.tavros:camel-restdsl-openapi-plugin:0.1.5:generate', '-DspecificationUri=' + specUri, '-f', generatedApiDirectory]
+def mvnCommand = ['mvn', 'com.ms3-inc.tavros:camel-restdsl-openapi-plugin:0.1.6:generate', '-DspecificationUri=' + specUri, '-f', generatedApiDirectory]
 
 if (System.properties['os.name'].toLowerCase().contains('windows')) {
     log.info "Executing command for Windows"
@@ -38,3 +65,11 @@ process.consumeProcessOutput( out, err )
 process.waitFor()
 if( out.size() > 0 ) println out
 if( err.size() > 0 ) println err
+
+if (process.exitValue() != 0) {
+    isFailure = true;
+}
+
+if (isFailure) {
+    throw new Exception("Build stopped because the post processing script ran into a problem.");
+}
