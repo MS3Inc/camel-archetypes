817d2ef6da9516e6660afc5c1a935c9070aeec92
R. L. Ratcliffe <rlr@tcliffe.dev>
2021-02-15 09:59:15 -0500

    Revert open api plugin version until it's public

--- camel-openapi-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ camel-openapi-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -51,7 +51,7 @@ def generatedApiDirectory = request.outputDirectory + "/" + request.artifactId
 log.info("Running mvn in " + generatedApiDirectory)
 
 def prefixForRunningWithWindows =  ['cmd', '/c']
-def mvnCommand = ['mvn', 'com.ms3-inc.tavros:camel-restdsl-openapi-plugin:0.1.6:generate', '-DspecificationUri=' + specUri, '-f', generatedApiDirectory]
+def mvnCommand = ['mvn', 'com.ms3-inc.tavros:camel-restdsl-openapi-plugin:0.1.5:generate', '-DspecificationUri=' + specUri, '-f', generatedApiDirectory]
 
 if (System.properties['os.name'].toLowerCase().contains('windows')) {
     log.info "Executing command for Windows"
